240d8cee6d588fbe04e92ec22f75611114f114f7
BieNew22 <newbie11004@gmail.com>
2024-09-28 23:04:57 +0900

    Feat: add user input validation logic

--- main.py
+++ main.py
@@ -12,8 +12,24 @@ def div(a, b):
     return a / b
 
 
-num1, num2 = map(int, input("두 수를 입력 >> ").split())
-op = input("연산 기호를 입력(+, -, *, /) >> ")
+num1, num2 = -1, -1
+while True:
+    try:
+        num1, num2 = map(int, input("두 정수를 공백을 두고 입력 >> ").split())
+
+        break
+    except:
+        print("=== error : 두 정수를 입력하셔야 합니다. ===")
+
+op_set = {'-', '*', '+', '/'}
+while True:
+    op = input("연산 기호를 입력(+, -, *, /) >> ")
+
+    if op not in op_set:
+        print("-, +, *, / 중 하나를 입력하세요!!")
+    else:
+        break
+    
 
 if op == "+":
     print(add(num1, num2))
