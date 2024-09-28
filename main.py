e524ef760702b4d4d76a9e05560b92bc0eb3e721
BieNew22 <newbie11004@gmail.com>
2024-09-28 22:59:32 +0900

    Feat: change result print - Plan 1

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ num1, num2 = map(int, input("두 수를 입력 >> ").split())
 op = input("연산 기호를 입력(+, -, *, /) >> ")
 
 if op == "+":
-    print(add(num1, num2))
+    print("result :", add(num1, num2))
 elif op == "-":
-    print(sub(num1, num2))
+    print("result :", sub(num1, num2))
 elif op == '*':
-    print(mul(num1, num2))
+    print("result :", mul(num1, num2))
 else:
-    print(div(num1, num2))
+    print("result :", div(num1, num2))
